49652853a1c6ffe41efc971b6dfeb693357acb43
Rafael7Smith <38447878+Rafael7Smith@users.noreply.github.com>
2019-01-17 18:14:41 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,26 @@
+function validateUser()
+{
+	//call function to ping database?
+	//if (databaseOnline) -> do login
+	//else -> error page, server offline
+	
+	//get username and password
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	
+	
+  //alert("success")
+	//alert(username)
+	//alert(password)
+	//sanitize inputs?
+	
+	//create package
+	
+	//response from stack
+	
+	//if(user good)
+	//direct user to contacts.html
+	
+	//else
+	//fail whale
+}
