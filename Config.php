e56207f7cd0cc57e8a83e1c104b0e846827d8963
Rafael7Smith <38447878+Rafael7Smith@users.noreply.github.com>
2019-01-26 18:39:55 -0500

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,7 +1,12 @@
 <?php
-	define('DB_SERVER', 'localhost:3036');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', 'password');
-	define('DB_DATABASE', 'database');
-	$dataBase = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	function getDataBase()
+	{
+		define('DB_SERVER', 'localhost:3306');
+		define('DB_USERNAME', 'webaccess');
+		define('DB_PASSWORD', 'Generic123!');
+		define('DB_DATABASE', 'cop4331');
+		$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		return $db;
+	}
+
 ?>
