eda2c753e97074edbc2fd143fb1e7d66b15e040f
Rafael7Smith <38447878+Rafael7Smith@users.noreply.github.com>
2019-01-26 19:34:12 -0500

    Add files via upload

--- SignUp.php
+++ SignUp.php
@@ -6,18 +6,21 @@
    {
       //Most of this is depracated junk, needs to be redone -soon TM(C)R
       
-      $username = mysqli_real_escape_string($dataBase,$_POST['username']);
-      $password = mysqli_real_escape_string($dataBase,$_POST['password']); 
-      $authpassword = mysqli_real_escape_string($dataBase,$_POST['password_confirm']);
+      $username = $_POST['username'];
+      $password = $_POST['password']; 
+      $authpassword = $_POST['password_confirm'];
 	  
 	  if($password != $authpassword)
 	  {
 		  echo "Passwords do not match";
+		  header("location: SignUp.html");
+		  exit();
+		  
 	  }
 	  
-      $sql = "INSERT INTO Users (Username,Password) VALUES (" . $username . ",'" . $password . "')";
-	  
-      if($result = mysqli_query($dataBase,$sql) == TRUE)
+      $sql = "INSERT INTO Users (Username,Password) VALUES ('" . $username . "','" . $password . "')";
+	  $conn = getDataBase();
+      if($result = mysqli_query($conn,$sql) == TRUE)
 	  {
 		  echo "New user registered";
 	  }
